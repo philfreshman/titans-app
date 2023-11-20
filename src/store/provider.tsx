7d86779381931300522d843bbd5bc4311c757f75
Philip Swiezak <philip.swiezak@brickbite.de>
2023-11-20 18:39:05 +0100

    add persistant storage

--- src/store/provider.tsx
+++ src/store/provider.tsx
@@ -1,12 +1,17 @@
 "use client";
 
 import { PropsWithChildren } from "react"
-import ReduxProvider from "@/src/store/reduxProvider"
+import { Provider } from 'react-redux'
+import {store, persistor} from "@/src/store/store"
+import {PersistGate} from "redux-persist/integration/react"
 
 export default function Providers({ children }: PropsWithChildren<any>) {
   return (
-    <ReduxProvider>
-      {children}
-    </ReduxProvider>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        {children}
+      </PersistGate>
+    </Provider>
   )
 }
+
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,35 @@
-import { configureStore } from '@reduxjs/toolkit'
-import rolesReducer from "@/src/store/slices/roleSlice"
-import rightsReducer from "@/src/store/slices/rightsSlice"
+"use client";
+
+import {combineReducers, configureStore} from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'
+import rolesReducer from "@/src/store/slices/roleSlice";
+import rightsReducer from "@/src/store/slices/rightsSlice";
+import {FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE} from "redux-persist/es/constants"
+
+// Configuration for redux-persist
+const persistConfig = {
+  key: 'root', // key is required
+  storage, // storage is required
+};
+
+const rootReducer = combineReducers({
+  roles: rolesReducer,
+  rights: rightsReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: {
-    roles: rolesReducer,
-    rights: rightsReducer,
-  }
-})
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
 
-export default store
+const persistor = persistStore(store);
 
+export { store, persistor };
